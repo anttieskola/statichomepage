493e516117cd757d13a3e7a04219d9afb1f7fc63
Antti Eskola <antti.eskola@live.com>
2021-10-16 20:25:11 +0300

    TS works out but

--- Presentation/Ui.Components/scripts/VisitorData.ts
+++ Presentation/Ui.Components/scripts/VisitorData.ts
@@ -1,5 +1,1 @@
-﻿// GetWindowNavigatorProperty
-// https://html.spec.whatwg.org/multipage/system-state.html#dom-navigator
-function GetWindowNavigatorProperty(propertyName) {
-    return window.navigator[propertyName];
-}
+﻿
---
+++ Presentation/Ui.Components/scripts/components.ts
@@ -0,0 +1,5 @@
+﻿// GetWindowNavigatorProperty
+// https://html.spec.whatwg.org/multipage/system-state.html#dom-navigator
+function GetWindowNavigatorProperty(propertyName) {
+    return window.navigator[propertyName];
+}
